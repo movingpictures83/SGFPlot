39f568a163f3160155d0486126438d740b2e3be8
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-11-06 19:32:59 -0500

    Initial

---
+++ SGFPlotPlugin.R
@@ -0,0 +1,25 @@
+## ---- echo = FALSE, results = 'hide'---------------------------------------
+library(knitr)
+opts_chunk$set(error = FALSE)
+
+## ----style, echo = FALSE, results = 'asis'---------------------------------
+##BiocStyle::markdown()
+
+## ---- message = FALSE------------------------------------------------------
+library(SGSeq)
+
+
+input <- function(inputfile) {
+   sgfc_ucsc <<- readRDS(inputfile)
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+## ----figure-1, fig.width=4.5, fig.height=4.5-------------------------------
+pdf(outputfile)
+
+
+plotFeatures(sgfc_ucsc, geneID = 1)
+}
+
